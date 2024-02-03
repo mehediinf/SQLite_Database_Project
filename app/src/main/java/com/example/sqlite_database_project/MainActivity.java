3240cab3fdcc5c4c1703522620a70a492dc9fbf0
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-04 01:06:24 +0600

    Second Commit SQLite_Database_Project

--- app/src/main/java/com/example/sqlite_database_project/MainActivity.java
+++ app/src/main/java/com/example/sqlite_database_project/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.sqlite_database_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+        MyDatabaseHelper myDatabaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDatabaseHelper = new MyDatabaseHelper(this);
+        SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
     }
 }
---
+++ app/src/main/java/com/example/sqlite_database_project/MyDatabaseHelper.java
@@ -0,0 +1,70 @@
+package com.example.sqlite_database_project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Student.db";
+    private static final String TABLE_NAME = "student_detail";
+    private static final String ID = "_id";
+    private static final String NAME = "Name";
+    private static final String AGE = "Age";
+    private static final String GENDER = "Gender";
+    private static final int VERSION_NUMBER = 2;
+
+    //Database Table Create Command
+    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+"("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+NAME+" VARCHAR(225),"+AGE+" INTEGER,"+GENDER+" VARCHAR(20) );";
+
+    //Database Table Drop Command
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " +TABLE_NAME;
+
+    private Context context;
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, VERSION_NUMBER);
+
+        this.context = context;
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+
+//এই খানে CREATE_TABLE variable call করার হইছে
+
+        try
+        {
+            Toast.makeText(context,"onCreate is called ",Toast.LENGTH_SHORT).show();
+            db.execSQL(CREATE_TABLE);
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(context,"Exception : "+e,Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            Toast.makeText(context,"onUpgrade is called ",Toast.LENGTH_SHORT).show();
+            db.execSQL(DROP_TABLE);
+            onCreate(db);
+
+        }catch (Exception e){
+
+            Toast.makeText(context,"Exception : "+e,Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
